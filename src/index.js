47bd877ac1c64834bdff836ded69d08b51ea1791
informagico <alessandro.magoga92@gmail.com>
2019-12-03 20:16:02 +0100

    Added Express with CORS.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import 'dotenv/config'
+'use strict'
 
-console.log('Hello World! This is my ENV variable: ' + process.env.TEST_ENV_VARIABLE)
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+	console.log('Hello World!')
+
+	res.sendStatus(200)
+})
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`Example app listening on port ${process.env.PORT}!`)
+})

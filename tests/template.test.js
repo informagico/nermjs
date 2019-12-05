38c3d052e7a88cbb9dc4a7d1b8d4351533c55396
informagico <alessandro.magoga92@gmail.com>
2019-12-05 20:39:55 +0100

    Removed babel (for now). Added test templates (jest+supertest).

---
+++ tests/template.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest')
+
+const app = require('../src/app')
+const Template = require('../src/models/template-model')
+
+test('Should test the test', async () => {
+	expect(1).toBe(1)
+})
